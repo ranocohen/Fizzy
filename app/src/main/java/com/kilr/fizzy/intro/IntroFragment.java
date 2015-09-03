d2824120b1a3699bf66972c59f7065141cc40db3
Ran Cohen <ranocohen@gmail.com>
2015-09-03 13:26:10 +0300

    intro general fragment added

---
+++ app/src/main/java/com/kilr/fizzy/intro/IntroFragment.java
@@ -0,0 +1,30 @@
+package com.kilr.fizzy.intro;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rancohen on 8/25/15.
+ */
+public class IntroFragment extends Fragment {
+
+    final static String LAYOUT_ID = "layoutid";
+
+    public static IntroFragment newInstance(int layoutId) {
+        IntroFragment pane = new IntroFragment();
+        Bundle args = new Bundle();
+        args.putInt(LAYOUT_ID, layoutId);
+        pane.setArguments(args);
+        return pane;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(getArguments().getInt(LAYOUT_ID, -1), container, false);
+        return rootView;
+    }
+
+}
