c754a2bef3bfc23c03e7e7f640c9b0dc9d6db466
Ran Cohen <ranocohen@gmail.com>
2015-09-25 17:11:50 +0300

    main activity basic cleanup

--- app/src/main/java/com/kilr/fizzy/utils/Utils.java
+++ app/src/main/java/com/kilr/fizzy/utils/Utils.java
@@ -1,7 +1,16 @@
 package com.kilr.fizzy.utils;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by rancohen on 9/25/15.
  */
 public class Utils {
+
+    public static LatLng geoPointFromLocation(Location loc) {
+        return new LatLng(loc.getLatitude(), loc.getLongitude());
+    }
+
 }
