dd3e99a34558f3001ebbacb14cfc93499739bb47
idanakav <idanakav@gmail.com>
2015-09-25 17:10:54 +0300

    cleanup

--- app/src/main/java/com/kilr/fizzy/FizzyApp.java
+++ app/src/main/java/com/kilr/fizzy/FizzyApp.java
@@ -3,10 +3,6 @@ package com.kilr.fizzy;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
-import com.kilr.fizzy.models.Message;
-import com.parse.Parse;
-import com.parse.ParseFacebookUtils;
-import com.parse.ParseObject;
 
 import timber.log.Timber;
 
@@ -21,12 +17,8 @@ public class FizzyApp extends Application {
 
         Timber.plant(new Timber.DebugTree());
         FacebookSdk.sdkInitialize(getApplicationContext());
-        ParseObject.registerSubclass(Message.class);
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
+        //Parse.initialize(this, "W37V8wslrioALql4uUGFJqxKwiRIhWLw3adCaR7l", "Wjjf2byorlAH9uGRsLfYyjH8X2SfGyjICEXz0mUn");
 
-        Parse.initialize(this, "W37V8wslrioALql4uUGFJqxKwiRIhWLw3adCaR7l", "Wjjf2byorlAH9uGRsLfYyjH8X2SfGyjICEXz0mUn");
-        ParseFacebookUtils.initialize(this);
 
     }
 
